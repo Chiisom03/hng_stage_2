24bba2202f9cdde43e897fb6be3c12a98ebcba67
Peter Chisom <peterchisom69@gmail.com>
2024-07-07 19:34:43 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar"
+
+function App() {
+
+  return (
+    <div className="bg-primary h-screen w-full font-cabin">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [ 
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        turquoise: 'var(--color-turquoise)',
+        't-orange': 'var(--color-t-orange)',
+      },
+      fontFamily: {
+         cabin: ['var(--default-font)'],
+      },
+      fontSize: {
+        'sm-text': 'var(--font-small)',
+        'md-text': 'var(--font-medium)',
+        'lg-text': 'var(--font-large)',
+      },
+    },
+    plugins: [],
+}
+}
