30c769e98e1b1b57138ccab457a4f9e3d6c74d1f
Peter Chisom <peterchisom69@gmail.com>
2024-07-10 00:10:49 +0100

    Auto commit at 2024-07-10 00:10:46

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -5,7 +5,7 @@ import OrderSummary from '../components/cart/order_summary';
 
 const CheckOutPage: React.FC = () => {
   return (
-    <div className='max-w-[1052px] mx-auto flex items-center'>
+    <div className='max-w-[1052px] mx-auto flex flex-col lg:flex-row items-center'>
       <BillingCardForm />
       <OrderSummary />
     </div>
