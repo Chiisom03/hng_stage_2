85bf7d4f6dfb1036f46cdf5d35e3f9370129dd07
Peter Chisom <peterchisom69@gmail.com>
2024-07-09 21:54:41 +0100

    Auto commit at 2024-07-09 21:54:41

--- src/components/section/by_products.tsx
+++ src/components/section/by_products.tsx
@@ -7,7 +7,17 @@ import HeartIcon from '../../assets/img/icons/heart_icon.svg'
 
 const ByProducts = () => {
 
-  const products = [
+
+  return (
+    <>
+      <MobileByProduct />
+      <DesktopByProduct />
+    </>
+  )
+}
+
+const MobileByProduct = () => {
+  const mobileProducts = [
     {
       name: 'COMELIA BRACLETS',
       price: 'N2,300,000',
@@ -58,13 +68,91 @@ const ByProducts = () => {
     },
   ]
   return (
-    <div className='lg:mb-[180px] mx-auto max-w-[1512px]'>
+    <div className='lg:hidden block lg:mb-[180px] mx-auto max-w-[1512px]'>
+      <h1 className='mx-4 uppercase mb-[68px] text-[54px]'>ARRIVALS</h1>
+
+      <div className="w-full mt-[68px] mb-[57px] overflow-x-scroll">
+        <div className='flex w-max h-max pb-8'>
+          {mobileProducts.map((product => (
+            <div key={product.name} className='flex flex-col mr-[18px] first:ml-4'>
+              <img src={product.image} alt="" className='w-[293px] h-[256px] rounded-primary' />
+              <div className='flex items-start justify-between mt-[24px]'>
+                <div>
+                  <p className='text-[26px] uppercase'>{product.name}</p>
+                  <p className='text-[22px] font-semibold'>{product.price}</p>
+                </div>
+                <div className='rounded-full py-[8px] pl-[8px] pr-[12px] bg-gray-100'>
+                  <img src={HeartIcon} alt="heart_icon" className='w-[42.86px] h-[42.86px]' />
+                </div>
+              </div>
+            </div>
+          )))}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+const DesktopByProduct = () => {
+
+  const desktopProducts = [
+    {
+      name: 'COMELIA BRACLETS',
+      price: 'N2,300,000',
+      isLiked: false,
+      image: ComeliaBracelets
+    },
+    {
+      name: 'Boho Earings',
+      price: 'N800,000',
+      isLiked: false,
+      image: BohoEarings
+    },
+    {
+      name: 'Galaxy Ring',
+      price: 'N1,000,000',
+      isLiked: false,
+      image: GalaxyRing
+    },
+    {
+      name: 'Kateryna',
+      price: 'N1,000,000',
+      isLiked: false,
+      image: Kateryna
+    },
+    {
+      name: 'COMELIA BRACLETS',
+      price: 'N2,300,000',
+      isLiked: false,
+      image: ComeliaBracelets
+    },
+    {
+      name: 'Boho Earings',
+      price: 'N800,000',
+      isLiked: false,
+      image: BohoEarings
+    },
+    {
+      name: 'Galaxy Ring',
+      price: 'N1,000,000',
+      isLiked: false,
+      image: GalaxyRing
+    },
+    {
+      name: 'Kateryna',
+      price: 'N1,000,000',
+      isLiked: false,
+      image: Kateryna
+    },
+  ]
+  return (
+    <div className='hidden lg:block lg:mb-[180px] mx-auto max-w-[1512px]'>
       <h1 className='lg:block hidden max-w-[1182px] mx-auto text-lg-text uppercase mb-[68px]'>By Products</h1>
       <h1 className='lg:hidden block max-w-[1182px] lg:mx-auto mx-4 uppercase mb-[68px] text-[54px]'>ARRIVALS</h1>
 
       <div className="w-full mt-[68px] mb-[57px] overflow-x-scroll">
         <div className='flex w-max h-max pb-8'>
-          {products.map((product => (
+          {desktopProducts.map((product => (
             <div key={product.name} className='flex flex-col'>
               <img src={product.image} alt="" className='w-[438px] h-[400px]' />
               <div className='flex items-start justify-between mt-[42px] ml-8'>
