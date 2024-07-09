6b4fec48119ce179f3ad3d9f7b168ccf332b54d0
Peter Chisom <peterchisom69@gmail.com>
2024-07-09 16:12:06 +0100

    Auto commit at 2024-07-09 16:12:05

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -3,6 +3,7 @@ import CartIcon from '../../assets/img/icons/cart_icon.svg';
 import HamIcon from '../../assets/img/icons/hamburger.svg';
 import Logo from '../../assets/img/logo.svg';
 import Button from '../commons/button';
+import { useState } from 'react';
 
 const Navbar = () => {
 
@@ -25,12 +26,14 @@ const Navbar = () => {
         }
     ]
 
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
-        <nav className="w-full lg:border-b border-white bg-primary">
+        <nav className="w-full lg:border-b border-white bg-primary relative">
             <div className="flex justify-between items-center py-[20px] max-w-[1182px] lg:mx-auto mx-4">
                 <img src={Logo} alt="logo" className='h-5 w-[100px]' />
                 <div className="flex gap-x-4 items-center">
-                    <ul className="lg:flex flex-col lg:flex-row divide-y-2 divide-primary lg:divide-y-0 bg-[#FBFBFB] lg:bg-transparent hidden">
+                    <ul className={`lg:flex lg:relative absolute top-0 left-0 w-full flex-col lg:flex-row divide-y-2 divide-primary lg:divide-y-0 bg-[#FBFBFB] lg:bg-transparent ${showMenu ? 'block' : 'hidden'}`}>
                         {navLinks.map((nav) => (
                             <li key={nav.title} className="mr-5 last:mr-[60px] text-md-text uppercase">
                                 <a href={nav.link}>{nav.title}</a>
@@ -43,9 +46,9 @@ const Navbar = () => {
                     <div className='flex justify-center items-center gap-x-5 lg:gap-x-[25px] lg:mr-8'>
                         <a href="/"><img src={SearchIcon} alt="Search Icon" className='icons' /></a>
                         <a href="/cart"> <img src={CartIcon} alt="Cart Icon" className='icons' /></a>
-                        <div className='block lg:hidden h-10 w-10 '>
+                        <button className='block lg:hidden h-10 w-10 ' onClick={() => setShowMenu(!showMenu)}>
                             <img src={HamIcon} alt="harmburger_menu" />
-                        </div>
+                        </button>
                     </div>
 
 
