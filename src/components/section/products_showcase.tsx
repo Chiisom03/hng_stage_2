14e45a2407681ef9e4cd0e41d8c928c3bba34b54
Peter Chisom <peterchisom69@gmail.com>
2024-07-10 14:12:17 +0100

    Auto commit at 2024-07-10 14:12:17

--- src/components/section/products_showcase.tsx
+++ src/components/section/products_showcase.tsx
@@ -3,11 +3,9 @@ import Earings from '../../assets/img/products/earings.png';
 import Rings from '../../assets/img/products/rings.png';
 import Watches from '../../assets/img/products/watches.png';
 import Button from '../commons/button';
-import { useNavigate } from 'react-router-dom';
 
 
 const ProductsShowcase = () => {
-  const navigate = useNavigate();
 
   const products = [
     {
@@ -33,18 +31,19 @@ const ProductsShowcase = () => {
   ];
 
   return (
-    <div className='mt-[200px] max-w-[1146px] lg:mx-auto mb-[120px] mx-4'>
-      <h1 className='lg:hidden block max-w-[1182px] mx-auto text-lg-text uppercase mb-[46px]'>By Products</h1>
+    <div className='mt-[200px] lg:max-w-[1182px] lg:mx-auto mb-[120px] px-4'>
+      <h1 className='lg:hidden block text-[54px] uppercase mb-[46px]'>By Products</h1>
 
-      <div className='lg:mt-[200px] flex'>
-        <div className="grid grid-cols-2 lg:gap-8 gap-4 lg:mr-[101px]">
+      <div className='lg:mt-[200px] flex justify-center items-center'>
+        <div className="grid grid-cols-2 lg:gap-x-8 lg:gap-y-8 gap-x-4 gap-y-[86px] lg:mr-[101px]">
           {products.map((product) => (
-            <div onClick={()=>{navigate(`/checkout/${product.id}`)}} key={product.id} >
-              <div className="h-max w-max bg-gray-100 lg:rounded-none rounded-[10px]'">
-                <img src={product.image} alt={product.name} className='lg:w-[376px] lg:h-[416px] bg-cover w-[190px] h-[280px] lg:rounded-none rounded-[10px]' />
+            <a href={`/checkout/${product.id}`} key={product.id} >
+              <div className="h-max w-max bg-gray-100 lg:rounded-none rounded-primary'">
+                <img src={product.image} alt={product.name}
+                  className='lg:w-[376px] lg:h-[416px] bg-cover w-[150px] h-[250px] lg:rounded-none rounded-[10px]' />
               </div>
               <p className='mt-6 lg:hidden text-[28px]'>{product.name}</p>
-            </div>
+            </a>
 
           ))}
         </div>
